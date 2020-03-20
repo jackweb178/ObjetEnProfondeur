a9f845bb1a512fb665c053a2be1ffe6b80071892
talibadio <jackweb178@gmail.com>
2020-03-20 22:32:26 +0000

    Parcourrir nos objets avec foreach()

--- MaClasse.php
+++ MaClasse.php
@@ -3,26 +3,46 @@
 
 class MaClasse
 {
-    //https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667055-les-objets-en-profondeur#/id/r-1670283
-    private static $instance =0;
+    //https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/1667055-les-objets-en-profondeur#/id/r-1667054
+    public  $attrbut1="premier attribut public";
+    public $attribut2="duexieme attribut public";
 
-    public function __construct()
+    private  $attrbutPrivate1="premier attribut private";
+    private $attributPrivate2="duexieme attribut private";
+
+    protected  $attrbutProtected1="premier attribut protected";
+    protected $attributProtected2="duexieme attribut protected";
+
+    function  listeAttribut()
     {
-        self::$instance++;
-    }
-
-    public function __clone()
-    {
-        self::$instance++;
-    }
-
-    public static function getInstance(){
-        return self::$instance;
+        foreach ($this as $attribut =>$valeur)
+        {
+            echo '<strong>',$attribut,'</strong> => ', $valeur,'<br/>';
+        }
     }
 }
 
-$a =new MaClasse();
-echo 'Le nombre dinstance est de  : ',MaClasse::getInstance(),'<br/>';// affiche 1
-$b= clone $a;
+class  Enfant extends MaClasse
+{
+    function listeAttribut()//redefinition de lafonction pour que ce ne soit pas celle de la classe mere qui soit appeler
+    {
+        foreach ($this as $attribut =>$valeur)
+        {
+            echo '<strong>',$attribut,'</strong> => ', $valeur,'<br/>';
+        }
+    }
+}
+$classe = new MaClasse() ;
+$enfant = new Enfant();
 
-echo 'Le nombre dinstance est de  : ',MaClasse::getInstance();// affiche 2
+echo '---- Liste les attributs depuis l\'intérieur de la classe principale ----<br />';
+$classe->listeAttribut();
+
+echo '<br />---- Liste les attributs depuis l\'intérieur de la classe enfant ----<br />';
+$enfant->listeAttribut();
+
+echo '<br />---- Liste les attributs depuis le script global ----<br />';
+foreach ($classe as $attribut =>$valeur)
+{
+    echo '<strong>',$attribut,'</strong> => ', $valeur,'<br/>';
+}
